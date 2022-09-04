1f0d0d78ab4ea31a610bd336cdd4834772d5668c
chris fotos <chrisfotos.developer@gmail.com>
2022-09-03 21:50:16 -0400

    fix typo in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-laoder'],
+				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
 		],
 	},
